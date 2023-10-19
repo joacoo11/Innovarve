474a1afdef8d947090c306e406408a61e018f2f1
Joaco Lobru <joacolobrutto11@gmail.com>
2023-10-19 12:31:05 -0300

    Update jgl-collection.js

--- assets/jgl-collection.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    var liElements = document.querySelector('li[data-collection="273557913754"]');
-    alert(liElements);
-    if(liElements) { // Check if the element exists to avoid null reference
-        liElements.classList.add('is-collapsed');
-    }
-});
